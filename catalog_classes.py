3845fbcc082f7f192bc0954955549045234f943a
Megan Splettstoesser <mspletts@fnal.gov>
2018-08-01 18:16:39 -0500

    Fix indentation (2)

--- catalog_classes.py
+++ catalog_classes.py
@@ -8,7 +8,7 @@ Store catalog headers and properties (e.g. injection percent, measure or true).
 class CoaddCatalog():
 	"""Declare headers for coadd catalogs .../coadd/{tile}_{band}_cat.fits. There is a separate catalog for each band."""
 
-        # Once matched, headers will have form 'hdr_1' or 'hdr_2' with a suffix (suf) #
+	# Once matched, headers will have form 'hdr_1' or 'hdr_2' with a suffix (suf) #
 	def __init__(self, inj_percent, inj, suf):
 		"""Declare headers for matched (via join=1and2) catalog. Declare descriptive constants.
 
@@ -74,92 +74,90 @@ class GalaxyTruthCatalog():
 			Refers to the order in which catalog was matched (via join=1and2) in ms_matcher (order set by STILTS parameters `in1` and `in2`). Allowed values: '_1' '_2'.
 		"""
 
-		 # For plot title #
+		# For plot title #
 		# `inj` forced True for truth catalogs #
 		if inj:
 			self.title_piece = '{}% Inj Gal Truth Cat'.format(inj_percent)
 		self.axlabel = 'true'
 		# Headers are the same as `MOFCatalog` class. Reproduced below for clarity in ms_plotter.py #
 		# Magnitude, is string of form '(mag_g, mag_r, mag_i, mag_z)' #
-	    	self.mag_hdr = 'cm_mag{}'.format(suf)
+		self.mag_hdr = 'cm_mag{}'.format(suf)
 		self.mag_axlabel = 'cm_mag_true'
-                # For error calculation #
-                self.mag_err_hdr = None
-                self.cm_flux_hdr = 'cm_flux{}'.format(suf)
-                self.cm_flux_cov_hdr = 'cm_flux_cov{}'.format(suf)
-                # Size. cm_T units: arcseconds squared. #
-                self.cm_t_hdr = 'cm_T'  + str(suf)
-                self.cm_t_err_hdr = 'cm_T_err'  + str(suf)
+		# For error calculation #
+		self.mag_err_hdr = None
+		self.cm_flux_hdr = 'cm_flux{}'.format(suf)
+		self.cm_flux_cov_hdr = 'cm_flux_cov{}'.format(suf)
+		# Size. cm_T units: arcseconds squared. #
+		self.cm_t_hdr = 'cm_T'  + str(suf)
+		self.cm_t_err_hdr = 'cm_T_err'  + str(suf)
 		self.cm_t_s2n_axlabel = 'cm_T_s2n_true'
-                # Flags #
-                self.flags_hdr = 'flags{}'.format(suf)
-                self.obj_flags_hdr = 'obj_flags{}'.format(suf)
-                self.psf_flags_hdr = 'psf_flags{}'.format(suf)
-                self.cm_flags_hdr = 'cm_flags{}'.format(suf)
-                self.cm_max_flags_hdr = 'cm_max_flags{}'.format(suf)
-                self.cm_flags_r_hdr = 'cm_flags_r{}'.format(suf)
+		# Flags #
+		self.flags_hdr = 'flags{}'.format(suf)
+		self.obj_flags_hdr = 'obj_flags{}'.format(suf)
+		self.psf_flags_hdr = 'psf_flags{}'.format(suf)
+		self.cm_flags_hdr = 'cm_flags{}'.format(suf)
+		self.cm_max_flags_hdr = 'cm_max_flags{}'.format(suf)
+		self.cm_flags_r_hdr = 'cm_flags_r{}'.format(suf)
 		self.cm_mof_flags_hdr = 'cm_mof_flags{}'.format(suf)
-                # For region file #
-                self.ra_hdr = 'ra{}'.format(suf)
-                self.dec_hdr = 'dec{}'.format(suf)
-                self.a_hdr, self.b_hdr = None, None
+		# For region file #
+		self.ra_hdr = 'ra{}'.format(suf)
+		self.dec_hdr = 'dec{}'.format(suf)
+		self.a_hdr, self.b_hdr = None, None
 		self.angle = None
-		# #
-		self.identifier = 'number{}'.format(suf)
 
 
 
-	
+
 class SOFCatalog():
-        """Declare headers for SOF catalogs.""" 
+	"""Declare headers for SOF catalogs.""" 
 
 	# Once matched, headers will have form 'hdr_1' or 'hdr_2' with a suffix (suf) #
 	def __init__(cat_type_pair, self, inj_percent, inj, suf):
 		"""Declare headers for matched (via join=1and2) catalog. Declare descriptive constants.
 
-                Parameters
-                ----------
+		Parameters
+		----------
 		inj (bool)
-                inj_percent (int)
+		inj_percent (int)
 		cat_type_pair (str)
-                suf (str)
+		suf (str)
 			Refers to the order in which catalog was matched (via join=1and2) in ms_matcher (order set by STILTS parameters `in1` and `in2`). Allowed values: '_1' '_2'.
-                """
+		"""
 
 		if inj:
 			self.title_piece = '{}% Inj SOF Cat'.format(inj_percent)
 		if inj is False:
-                        self.title_piece = 'Base SOF Cat'
+			self.title_piece = 'Base SOF Cat'
 
 		self.axlabel = 'meas'
-                # Headers are the same as MOFCatalog class with the exception of cm_mof_flags_hdr. Reproduced below for clarity #
+		# Headers are the same as MOFCatalog class with the exception of cm_mof_flags_hdr. Reproduced below for clarity #
 		# Magnitude, is string of form '(mag_g, mag_r, mag_i, mag_z)' #
 		self.mag_hdr = 'cm_mag{}'.format(suf)
 		self.mag_axlabel = 'cm_mag_meas'
 		if cat_type_pair == 'star_truth':
 			self.mag_hdr = 'psf_mag{}'.format(suf)
 			self.mag_axlabel = 'psf_mag_meas'
-                # For error calculation #
-                self.mag_err_hdr = None
+		# For error calculation #
+		self.mag_err_hdr = None
 		#TODO there is psf_flux 
-                self.cm_flux_hdr = 'cm_flux{}'.format(suf)
-                self.cm_flux_cov_hdr = 'cm_flux_cov{}'.format(suf)
-                # Size #
-                self.cm_t_hdr = 'cm_T'  + str(suf)
-                self.cm_t_err_hdr = 'cm_T_err'  + str(suf)
-                self.cm_t_s2n_axlabel = 'cm_T_s2n_meas'
+		self.cm_flux_hdr = 'cm_flux{}'.format(suf)
+		self.cm_flux_cov_hdr = 'cm_flux_cov{}'.format(suf)
+		# Size #
+		self.cm_t_hdr = 'cm_T'  + str(suf)
+		self.cm_t_err_hdr = 'cm_T_err'  + str(suf)
+		self.cm_t_s2n_axlabel = 'cm_T_s2n_meas'
 		# Flags #
-                self.flags_hdr = 'flags{}'.format(suf)
-                self.obj_flags_hdr = 'obj_flags{}'.format(suf)
-                self.psf_flags_hdr = 'psf_flags{}'.format(suf)
-                self.cm_flags_hdr = 'cm_flags{}'.format(suf)
-                self.cm_max_flags_hdr = 'cm_max_flags{}'.format(suf)
-                self.cm_flags_r_hdr = 'cm_flags_r{}'.format(suf)
-                self.cm_mof_flags_hdr = None
-                # For region file #
-                self.ra_hdr = 'ra{}'.format(suf)
-                self.dec_hdr = 'dec{}'.format(suf)
-                self.a_hdr, self.b_hdr = None, None
+		self.flags_hdr = 'flags{}'.format(suf)
+		self.obj_flags_hdr = 'obj_flags{}'.format(suf)
+		self.psf_flags_hdr = 'psf_flags{}'.format(suf)
+		self.cm_flags_hdr = 'cm_flags{}'.format(suf)
+		self.cm_max_flags_hdr = 'cm_max_flags{}'.format(suf)
+		self.cm_flags_r_hdr = 'cm_flags_r{}'.format(suf)
+		self.cm_mof_flags_hdr = None
+		# For region file #
+		self.ra_hdr = 'ra{}'.format(suf)
+		self.dec_hdr = 'dec{}'.format(suf)
+		self.a_hdr, self.b_hdr = None, None
 		self.angle = None
 
 
@@ -172,13 +170,13 @@ class MOFCatalog():
 	def __init__(self, inj_percent, inj, suf):
 		"""Declare headers for matched (via join=1and2) catalog. Declare descriptive constants.
 
-                Parameters
-                ----------
-                inj_percent (int)
+		Parameters
+		----------
+		inj_percent (int)
 		inj (bool)
-                suf (str)
+		suf (str)
 			Refers to the order in which catalog was matched (via join=1and2) in ms_matcher (order set by STILTS parameters `in1` and `in2`). Allowed values: '_1' '_2'.
-                """
+		"""
 
 		# For plot title #
 		if inj:
@@ -199,11 +197,11 @@ class MOFCatalog():
 		self.cm_t_s2n_axlabel = 'cm_T_s2n_meas'
 		# Flags #
 		self.flags_hdr = 'flags{}'.format(suf)
-                self.obj_flags_hdr = 'obj_flags{}'.format(suf)
-                self.psf_flags_hdr = 'psf_flags{}'.format(suf)
-                self.cm_flags_hdr = 'cm_flags{}'.format(suf)
-                self.cm_max_flags_hdr = 'cm_max_flags{}'.format(suf)
-                self.cm_flags_r_hdr = 'cm_flags_r{}'.format(suf)
+		self.obj_flags_hdr = 'obj_flags{}'.format(suf)
+		self.psf_flags_hdr = 'psf_flags{}'.format(suf)
+		self.cm_flags_hdr = 'cm_flags{}'.format(suf)
+		self.cm_max_flags_hdr = 'cm_max_flags{}'.format(suf)
+		self.cm_flags_r_hdr = 'cm_flags_r{}'.format(suf)
 		self.cm_mof_flags_hdr = 'cm_mof_flags{}'.format(suf)
 		# For region file #
 		self.ra_hdr = 'ra{}'.format(suf)
@@ -215,19 +213,19 @@ class MOFCatalog():
 
 
 class StarTruthCatalog(): 
-        """Declare headers for star truth catalogs."""
+	"""Declare headers for star truth catalogs."""
 
 	# Once matched, headers will have form 'hdr_1' or 'hdr_2' with a suffix (suf) #
 	def __init__(self, inj_percent, inj, suf):
-                """Declare headers for matched (via join=1and2) catalog. Declare descriptive constants.
+		"""Declare headers for matched (via join=1and2) catalog. Declare descriptive constants.
 
-                Parameters
-                ----------
-                inj_percent (int)
+		Parameters
+		----------
+		inj_percent (int)
 		inj (bool)
-                suf (str)
+		suf (str)
 			Refers to the order in which catalog was matched (via join=1and2) in ms_matcher (order set by STILTS parameters `in1` and `in2`). Allowed values: '_1' '_2'.
-                """	
+		"""	
 
 		# `inj` forced True for truth catalogs #	
 		if inj:
@@ -237,10 +235,10 @@ class StarTruthCatalog():
 		self.mag_hdr = 'g_Corr{}'.format(suf) 
 		self.mag_axlabel = 'mag_true' 
 		# For error calculation #
-                # Is of form 'PSF_MAG_ERR_{band}{}'.format(suf) #
+		# Is of form 'PSF_MAG_ERR_{band}{}'.format(suf) #
 		self.mag_err_hdr = 'PSF_MAG_ERR{}'.format(suf)
-                self.cm_flux_hdr = None
-                self.cm_flux_cov_hdr = None
+		self.cm_flux_hdr = None
+		self.cm_flux_cov_hdr = None
 		# Size #
 		self.cm_t_hdr = None
 		self.cm_t_err_hdr = None
@@ -266,22 +264,22 @@ class Y3GoldCatalog():
 	"""Declare headers for Y3 Gold catalogs (https://cdcvs.fnal.gov/redmine/projects/des-y3/wiki/Full_bins_of_Y3_GOLD_2_0_Columns).""" 
 
 	# Once matched, headers will have form 'hdr_1' or 'hdr_2' with a suffix (suf) #
-	def __init__(self, inj_percent, inj, suf, y3_fit, y3_model, cat_type_pair):
-                """Declare headers for matched (via join=1and2) catalog. Declare descriptive constants.
+	def __init__(self, inj_percent, inj, suf, y3_fit, y3_model, cat_type_pair, cat_type):
+		"""Declare headers for matched (via join=1and2) catalog. Declare descriptive constants.
 
-                Parameters
-                ----------
-                inj_percent (int)
+		Parameters
+		----------
+		inj_percent (int)
 
-                inj (bool)
+		inj (bool)
 
 		y3_fit (str)
 			e.g. `mof` or `sof`.
 		y3_model (str)
 			e.g. `cm` or `psf`
-                suf (str)
-                        Refers to the order in which catalog was matched (via join=1and2) in ms_matcher (order set by STILTS parameters `in1` and `in2`). Allowed values: '_1' '_2'.
-                """
+		suf (str)
+			Refers to the order in which catalog was matched (via join=1and2) in ms_matcher (order set by STILTS parameters `in1` and `in2`). Allowed values: '_1' '_2'.
+		"""
 
 
 		if cat_type_pair == 'star_truth':
@@ -290,41 +288,41 @@ class Y3GoldCatalog():
 			y3_model = 'CM'
 
 		print 'Using {} and {} for Y3 Gold catalog...'.format(y3_fit, y3_model)
-	
+
 		if cat_type == 'y3_gold_2_2':
 			self.title_piece = 'Y3 Gold 2-2 Cat' 
 		if cat_type == 'y3_gold_2_0':
-                        self.title_piece = 'Y3 Gold 2-0 Cat'
+			self.title_piece = 'Y3 Gold 2-0 Cat'
 
-                self.axlabel = 'meas'
+		self.axlabel = 'meas'
 
 		# Note: band dependent headers -- MAG, MAG_ERR, FLUX, FLUX_COV, PSF_FLAGS #
 
-                # Magnitude # 
+		# Magnitude # 
 		self.mag_hdr = '{}_{}_MAG{}'.format(y3_fit, y3_model, suf).upper()
-                self.mag_axlabel = 'mag_meas'
-                # For error calculation #
+		self.mag_axlabel = 'mag_meas'
+		# For error calculation #
 		self.mag_hdr = '{}_{}_MAG_ERR{}'.format(y3_fit, y3_model, suf).upper()
 		self.cm_flux_hdr = '{}_{}_FLUX{}'.format(y3_fit, y3_model, suf).upper()
 		self.cm_flux_cov_hdr = '{}_{}_FLUX_COV{}'.format(y3_fit, y3_model, suf).upper()
-                # Size #
+		# Size #
 		self.cm_t_hdr = '{}_{}_T{}'.format(y3_fit, y3_model, suf).upper()
 		self.cm_t_err_hdr = '{}_{}_T_ERR{}'.format(y3_fit, y3_model, suf).upper()
 		self.cm_t_s2n_axlabel = 'cm_T_s2n_meas'
-                # Flags #
-                self.flags_hdr = 'FLAGS_GOLD{}'.format(suf).upper()
+		# Flags #
+		self.flags_hdr = 'FLAGS_GOLD{}'.format(suf).upper()
 		self.obj_flags_hdr = '{}_OBJ_FLAGS{}'.format(y3_fit, suf).upper()
-                self.psf_flags_hdr = '{}_PSF_FLAGS{}'.format(y3_fit, suf).upper()
-                self.cm_flags_hdr = '{}_{}_FLAGS{}'.format(y3_fit, y3_model, suf).upper()
-                self.cm_max_flags_hdr = None
+		self.psf_flags_hdr = '{}_PSF_FLAGS{}'.format(y3_fit, suf).upper()
+		self.cm_flags_hdr = '{}_{}_FLAGS{}'.format(y3_fit, y3_model, suf).upper()
+		self.cm_max_flags_hdr = None
 		# MOF_CM_MOF_FLAGS has no SOF equivalent #
 		# Note: is duplicate of `cm_flags_hdr` #
-                self.cm_mof_flags_hdr = '{}_{}_FLAGS{}'.format(y3_fit, y3_model, suf).upper()
-                self.cm_flags_r_hdr = '{}_{}_FLAGS_R{}'.format(y3_fit, y3_model, suf).upper()
-                # For region file #
+		self.cm_mof_flags_hdr = '{}_{}_FLAGS{}'.format(y3_fit, y3_model, suf).upper()
+		self.cm_flags_r_hdr = '{}_{}_FLAGS_R{}'.format(y3_fit, y3_model, suf).upper()
+		# For region file #
 		# Note: there is also an ALPHAWIN_J2000 and DELTAWIN_J2000 #
 		self.ra_hdr = 'ra{}'.format(suf).upper()
-                self.dec_hdr = 'dec{}'.format(suf).upper()
-                self.a_hdr = 'A_IMAGE{}'.format(suf).upper()
+		self.dec_hdr = 'dec{}'.format(suf).upper()
+		self.a_hdr = 'A_IMAGE{}'.format(suf).upper()
 		self.b_hdr = 'B_IMAGE{}'.format(suf).upper()
 		self.angle = 'THETA_J2000{}'.format(suf).upper()
