47a345e4522aed9232ac4e7b042ce7ac32de6efc
Megan Splettstoesser <mspletts@fnal.gov>
2018-08-02 12:20:05 -0500

    Add set_plot_and_catalog_constants

--- __init__.py
+++ __init__.py
@@ -5,5 +5,6 @@ Imports
 from . import catalog_classes
 from . import calculate_injection_percent
 from . import get_catalog_headers
-from . import get_catalog set_plot_and_catalog_constants
+from . import get_catalog 
+from . import set_plot_and_catalog_constants
 from . import ms_plotter
