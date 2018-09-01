ae242b9cb4753f87bef30bb5262b7fa78effa87f
Megan Splettstoesser <mspletts@fnal.gov>
2018-09-01 13:59:05 -0500

    Complete docstrings

--- region_files.py
+++ region_files.py
@@ -1,5 +1,9 @@
 """
-Make region files.
+Make DS9 region files.
+Three region files are created:
+(1) a region file for the objects in both catalogs specified in `set_constants.py`,
+(2) a region file for the objects uniquely in the catalog set by `MATCH_CAT1` (`INJ1`, `INJ1_PERCENT`, etc) and not `MATCH_CAT2`
+(3) a region file for the objects uniquely in the catalog set by `MATCH_CAT2` and not `MATCH_CAT1`.
 
 Comments are ABOVE the code they refer to.
 """
@@ -19,30 +23,56 @@ REGION_COLORS = {'1and2':'green', '1not2':'blue', '2not1':'green'}
 
 
 def write_region_file_line(ra, dec, fn, color, shape, overwrite, a=None, b=None, radius=None, angle=None):
-    """Write to a single region file.
+    """Write to a single DS9 region file.
+    J2000 coordinate system is assumed.
 
     Parameters
     ----------
-    a (1D array_like)
-        Semi-major axis. Only considered if `shape=ellipse`.
+    ra (1D array_like, dtype=float)
+        Right ascension of each object. Units: degrees.
 
-    b (1D array_like)
-                Semi-minor axis. Only considered if `shape=ellipse`.
+    dec (1D array_like, dtype=float)
+        Declination of each object. Units: degrees.
 
-    radius (1D array_like)
-        Only considered if `shape=circle`.
-
-    shape (str)
-        Allowed values: 'circle', 'ellipse'
-
-    ra, dec (1D array_like)
+    fn (str)
+        Complete filename for the region file.
 
     color (str)
+        Color for regions.
+        Allowed values: see http://ds9.si.edu/doc/ref/region.html#RegionProperties
+            
+    shape (str)
+        Shape of each region.
+        Allowed values: 'cirlce', 'ellipse'.
+
+    overwrite (bool)
+        If `True` the region file (given by `fn`) is overwritten if it already exists.
+        If `False` the region file (given by `fn`) is not overwritten if it already exists.  
+
+    a (1D array_like, dtype=float)
+        Semi-major axis of each object. Units: arcseconds.
+        Note that `A_IMAGE` and `B_IMAGE` are in units of pixels, but the conversion from DECam pixels to arcseconds is 1:1.
+        Only considered if `shape=ellipse`.
+
+    b (1D array_like, dtype=float)
+        Semi-minor axis of each object. Units: arcseconds.
+        Note that `A_IMAGE` and `B_IMAGE` are in units of pixels, but the conversion from DECam pixels to arcseconds is 1:1.
+        Only considered if `shape=ellipse`.
+
+    angle (1D array_like, dtype=float)
+        Rotation angle of each object. Units: degrees. 
+        Note that `THETA_IMAGE = THETA_J2000-90`
+        Only considered if `shape=ellipse`.
+
+    radius (1D array_like, dtype=float)
+        Radius of each object. Units: arcseconds.
+        Only considered if `shape=circle`.
+        If `shape=cirlce` and `radius=None` a default radius of 2 arcseconds is used.
 
     Returns
     -------
     fn (str)
-        Complete filename.
+        Complete filename of the region file that has been written or rewritten to.
     """
 
     if VERBOSE_ING: print 'Writing to region file...'
@@ -77,13 +107,54 @@ def write_region_file_line(ra, dec, fn, color, shape, overwrite, a=None, b=None,
 
 
 def write_region_files(df_1and2, df_1not2, df_2not1, realization, tile, balrog_run, output_directory):
-    """Write to DS9 region files.
+    """Write to three DS9 region files:
+    (1) region file that contains objects in both `MATCH_CAT1` and `MATCH_CAT2`
+    (2) region file that contains objects uniquely in `MATCH_CAT1` and not in `MATCH_CAT2`
+    and (3) region file that contains objects uniquely in `MATCH_CAT2` and not in `MATCH_CAT1`.
 
     Parameters
     ----------
+    df_1and2 (pandas DataFrame)
+        DataFrame that contains objects in both `MATCH_CAT1` and `MATCH_CAT2`.
+        This catalog is created in `stilts_matcher` using `join=1and2`.
+
+    df_1not2 (pandas DataFrame)
+        DataFrame that contains objects uniquely in `MATCH_CAT1` and not `MATCH_CAT2`.
+        This catalog is created in `stilts_matcher` using `join=1not2`.
+
+    df_2not1 (pandas DataFrame)
+        DataFrame that contains objects uniquely in `MATCH_CAT2` and not `MATCH_CAT1`.
+        This catalog is created in `stilts_matcher` using `join=2not1`.
+
+    realization (str)
+        A realization refers to a Balrog injection.
+        Realizations are numbered starting with 0. Realization can be 'None' (str).
+        The realization will be applied to `MATCH_CAT1` if `INJ1=True`, and `MATCH_CAT2` if `INJ2=True`.
+        If both `INJ1=False` and `INJ2=False` this should be set to `None` at the command line.
+        Note that although `realization` is an int, it is of type str because it is set using `sys.argv[]` (whose contents are strings).
+
+    tile (str)
+        A sky area unit used by DESDM [Dark Energy Survey Data Management] to parcel the DES footprint and organize the coadd outputs. Each tile is 0.7306 degrees on a side'. 
+        See Appendix A of https://arxiv.org/abs/1801.03181
+
+    balrog_run (str)
+        Names the Balrog test. This is a substring of `BASE_PATH_TO_CATS` entered at the command line.
+        Example: `base_path_to_catalogs=/data/des71.a/data/kuropat/des2247-4414_sof/` --> `balrog_run=des2247-4414_sof`.
+
+    output_directory (str)
+        All outputs of BalVal will be saved in `output_directory`.
+        For specific directory structure, see https://github.com/spletts/BalVal/blob/master/README.md#condensed-output-directory-structure
 
     Returns
     -------
+    fnRegion1and2 (str)
+        Complete filename for the region file that contains objects in both `MATCH_CAT1` and `MATCH_CAT2`.
+
+    fnRegion1not2 (str)
+        Complete filename for the region file that contains objects uniquely in `MATCH_CAT1` and not `MATCH_CAT2`.
+
+    fnRegion2not1 (str)
+        Complete filename for the region file that contains objects uniquely in `MATCH_CAT2` and not `MATCH_CAT1`.
     """
 
     __overwrite = False
@@ -153,7 +224,6 @@ def write_region_files(df_1and2, df_1not2, df_2not1, realization, tile, balrog_r
 
         fnRegion1and2 = write_region_file_line(ra=ra_1and2, dec=dec_1and2, fn=fnRegion1and2, color='green', shape='ellipse', a=a_1and2, b=b_1and2, radius=None, angle=orientation_1and2, overwrite=__overwrite)
         fnRegion2not1 = write_region_file_line(ra=ra_2not1, dec=dec_2not1, fn=fnRegion2not1, color='yellow', shape='ellipse', a=a_2not1, b=b_2not1, radius=None, angle=orientation_2not1, overwrite=__overwrite)
-
 
 
     return fnRegion1and2, fnRegion1not2, fnRegion2not1 
